3b305c190fce058997a6e4408bf21eefe3324829
Camilo Fernandez <cfernandeza822@ulacit.ed.cr>
2022-03-11 00:11:14 -0600

    Adicione cosas a js

--- javascript/autenticacion.js
+++ javascript/autenticacion.js
@@ -1,2 +1,19 @@
-variable = "camilo";
-apellido = "fernandez"
+var attempt = 3;
+
+function validacion(){
+    var usuario     = document.getElementById("usuario").value;
+    var contrasenna = document.getElementById("password").value;
+    if(usuario=="camiargu"&&contrasenna=="admin01"){
+        alert("Ha accedido al sistema");
+    } 
+    else {
+        attempt = attempt-1;
+        alert("Usted tiene "+attempt+" restantes");
+    }
+    return attempt;
+}
+
+function llamada(){
+    attempt = validacion();
+
+}
